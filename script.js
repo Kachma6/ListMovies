e8f1b8ab9fb3cf6a2ee777835dab682bee3026c0
Kachma6 <karenchoquecallata6@gmail.com>
2025-01-20 22:04:58 -0400

    Fix function listMovies

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 import { getMovies , postMovie} from "./firebase/config.js";
-const ctnMovies = document.getElementById("ctn-movies")
+
 const registerMovie = document.getElementById("register-movie")
 
 const listMovies = async () => {
-   
+    removeList()
     let movies = await getMovies()
     console.log(movies)
+    const ctnMovies = document.getElementById("ctn-movies")
     movies.forEach(item => {
         const listItem = document.createElement("div");
         listItem.textContent = `Nombre: ${item.name}`;
         ctnMovies.appendChild(listItem);
       });
 }
+const removeList = () =>{
+    const ctnMovies = document.getElementById("ctn-movies")
+    ctnMovies.remove()
+    const newCtn = document.createElement("div");
+    newCtn.setAttribute("id" , "ctn-movies")
+    const body = document.querySelector("body");
+    body.appendChild(newCtn)
+}
+
 const saveMovie = async (movie) => {
   let movieRegister = await postMovie(movie)
 }
