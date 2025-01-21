fe9de03da292940216fdcdcf528093d5368807a4
Kachma6 <karenchoquecallata6@gmail.com>
2025-01-21 10:43:17 -0400

    add

--- script.js
+++ script.js
@@ -33,3 +33,17 @@ registerMovie.addEventListener("submit", (e)=>{
   listMovies()
 })
 listMovies()
+
+
+// ------------------------------------------------
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/service-worker.js")
+          .then((registration) => {
+              console.log("Service Worker registrado con éxito:", registration);
+          })
+          .catch((error) => {
+              console.error("Error al registrar el Service Worker:", error);
+          });
+  });
+}
---
+++ serviceWorker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "mi-pwa-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/script.js",
+    "/firebase/config.js",
+    "/images/icon.jpg"
+   
+];
+
+// Instalación del Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Activación del Service Worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Interceptar solicitudes y servir desde caché
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
